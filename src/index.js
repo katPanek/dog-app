357e9f28a7588f462827951aa13f800a2be76793
katPanek <panekkata@gmail.com>
2022-06-23 19:15:06 +0200

    Fix router for github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { App } from './App';
@@ -10,13 +10,13 @@ import { BreedsTable } from './routes/BreedsTable';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={'/dog-app'}>
       <Routes>
         <Route path="/" element={<App />}>
+          <Route path="/" element={<BreedsTable />} />
           <Route path=":breedName" element={<Breed />} />
-          <Route path="/breeds-table" element={<BreedsTable />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
