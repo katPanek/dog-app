777608f57cd3dabfb0992467146dc53970758338
katPanek <panekkata@gmail.com>
2022-06-23 19:35:20 +0200

    Remove basename from HashRouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BreedsTable } from './routes/BreedsTable';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={'/dog-app'}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/" element={<BreedsTable />} />
